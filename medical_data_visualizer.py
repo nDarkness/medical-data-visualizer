4eebf6d8efea45e9c9de092a99b0757c95d62c63
nDarkness <ap@ndarkness.com>
2025-03-31 20:36:15 -0400

    feat:added step 3 fix:added fix for the instructions not to indent

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -11,7 +11,8 @@ df = pd.read_csv("medical_examination.csv")
 df['overweight'] = (df["weight"] / (df["height"] / 100) ** 2 > 25).astype(int)
 
 # 3
-
+df["gluc"] = (df["gluc"] > 1).astype(int)
+df["cholesterol"] = (df["cholesterol"] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
